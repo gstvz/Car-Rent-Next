6e350ce9379f1e3543ee35910ee7c9dea079ad26
Gustavo França <gstvz.franca@gmail.com>
2022-04-11 13:48:43 -0300

    feat: implemented ThemeProvider and GlobalStyle on _app; added Header to index page; deleted globals and Home css files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle, theme } from "@styles";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
