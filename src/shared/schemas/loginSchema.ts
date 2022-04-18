1a91b8d656774aee51ff2b0e281adb01ae949629
Gustavo França <gstvz.franca@gmail.com>
2022-04-18 15:30:30 -0300

    refactor: changed login and register schemas names to loginSchema and registerSchema, to avoid conflict with react-hook-form

--- src/shared/schemas/loginSchema.ts
+++ src/shared/schemas/loginSchema.ts
@@ -1,6 +1,6 @@
 import * as yup from "yup";
 
-export const login = yup.object({
+export const loginSchema = yup.object({
   email: yup.string().email("Invalid email.").required("Required field."),
   password: yup
     .string()
