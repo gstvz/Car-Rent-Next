e5c9dd3644c45219bb4373c84d6bfcf581f3efa7
Gustavo França <gstvz.franca@gmail.com>
2022-04-19 15:06:02 -0300

    Revert "Merge pull request #2 from gstvz/feat/spinners"
    This reverts commit 28d965df970d27c8e8de2c93238bf0a456313a36, reversing
    changes made to 30be16a4f0adfd9c075aaa185d49a3658fba22f7.

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,6 +1,5 @@
 export * from "./useActionButton";
 export * from "./useHover";
-export * from "./useIsLoading";
 export * from "./useRedirectToCar";
 export * from "./useScrollToTop";
 export * from "./useSlide";
